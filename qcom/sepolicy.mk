9f17d4ab9b273639d0fbdce8a5791b7f80028421
Nicholas Flintham <nick@flinny.org>
2018-02-12 16:09:33 +0000

    sepolicy: fixup .mk files
    Change-Id: Icf2dc919aaa422b8ab51e3399aca165813400ddd

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -2,6 +2,6 @@
 # This policy configuration will be used by all qcom products
 # that inherit from Slim
 #
-
 BOARD_SEPOLICY_DIRS += \
-    device/slim/sepolicy/qcom
+    device/slim/sepolicy/qcom/common \
+    device/slim/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
