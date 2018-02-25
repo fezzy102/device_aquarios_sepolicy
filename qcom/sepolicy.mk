250b755ee5cac06e5c8c64f27a087e9c81355b96
Adrian DC <radian.dc@gmail.com>
2018-02-25 19:00:02 +0100

    sepolicy: Isolate qcom sysfs_graphics policy for recovery
     * The sysfs_graphics context is not defined globally
        but only by qcom common sepolicies
    
     * Introduced by commit:
        sepolicy: Initial policy for recovery
        Change-Id: Iac57b472dd4628b9bd27fbe47fbbb35c09743be2
        Date: Sat, 17 Feb 2018 01:17:34 +0100
        From: Tom Marshall <tdm.code@gmail.com>
    
    Change-Id: If518dbd224f6993ecf06394cfe2bca022e1649fb

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -2,6 +2,10 @@
 # This policy configuration will be used by all qcom products
 # that inherit from Slim
 #
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/slim/sepolicy/qcom/private
+
 BOARD_SEPOLICY_DIRS += \
     device/slim/sepolicy/qcom/common \
     device/slim/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
